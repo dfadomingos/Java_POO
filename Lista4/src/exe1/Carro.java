e48f617ca9ff821eace9214b7f18b8a36db33bbf
dfadomingos <dfadomingos47@gmail.com>
2025-09-17 14:32:45 -0300

    exe1

---
+++ Lista4/src/exe1/Carro.java
@@ -0,0 +1,34 @@
+package exe1;
+
+public class Carro extends Veiculo{
+    private int portas;
+
+    public Carro(){
+        super();
+    }
+
+    public Carro(String placa, String modelo, double valorBase, int portas) {
+        super(placa, modelo, valorBase);
+        this.portas = portas;
+    }
+
+    public int getPortas() {
+        return portas;
+    }
+
+    public void setPortas(int portas) {
+        this.portas = portas;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                super.toString() + "portas=" + portas +
+                '}';
+    }
+
+    @Override
+    public double calcularImposto() {
+        return this.valorBase * 0.03f;
+    }
+}
