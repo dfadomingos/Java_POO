020a0ee9bddba4af5ab9a112ece0d966e9305d3f
dfadomingos <dfadomingos47@gmail.com>
2025-10-21 10:58:45 -0300

    post e delete - api

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(DemoApplication.class, args);
     }
 }
--- demo/src/main/java/com/example/demo/model/Produto.java
+++ demo/src/main/java/com/example/demo/model/Produto.java
@@ -1,4 +1,52 @@
 package com.example.demo.model;
 
 public class Produto {
+    private int id;
+    private String nome;
+    private float preco;
+    private int qtde;
+
+    public Produto() {
+    }
+
+    public Produto(int id, String nome, float preco, int qtde) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.qtde = qtde;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public int getQtde() {
+        return qtde;
+    }
+
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+
+
 }
